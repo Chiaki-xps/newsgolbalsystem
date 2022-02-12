8729f708f873c1886c47a056c19aae28dc8237aa
chiaki-xps <1490295446@qq.com>
2022-02-12 23:19:30 +0800

    09-TopHeader组件和修改antd版本

--- src/components/sandbox/TopHeader.jsx
+++ src/components/sandbox/TopHeader.jsx
@@ -1,26 +1,48 @@
 import React, { useState } from 'react'
-import { Layout } from 'antd';
+import { Layout, Dropdown, Menu, Avatar } from 'antd';
 import {
   MenuUnfoldOutlined,
-  MenuFoldOutlined
+  MenuFoldOutlined,
+  UserOutlined
 } from '@ant-design/icons';
 
 const { Header } = Layout;
 
 export default function TopHeader() {
-  const [collapsed] = useState(false)
-  return (
-    <Header className="site-layout-background" style={{ padding: 0 }}>
+  const [collapsed, setCollapsed] = useState(false)
+  const changeCollapsed = () => {
+    setCollapsed(!collapsed)
+  }
 
+  const menu = (
+    <Menu>
+      <Menu.Item>
+        超级管理员
+      </Menu.Item>
+      <Menu.Item danger>退出</Menu.Item>
+    </Menu>
+  );
+
+  return (
+    <Header className="site-layout-background" style={{ padding: '0 16px' }}>
+      
       {/* 相当于一个三目选择渲染组件MenuUnfoldOutlined或者MenuFoldOutlined */}
       {/* {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
               className: 'trigger',
               onClick: this.toggle,
       })} */}
       {/* 等价于 */}
+
       {
-        collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />
+        collapsed ? <MenuUnfoldOutlined onClick={changeCollapsed} /> : <MenuFoldOutlined onClick={changeCollapsed} />
       }
+
+      <div style={{ float: "right" }}>
+        <span>欢迎admin回来</span>
+        <Dropdown overlay={menu}>
+          <Avatar size="large" icon={<UserOutlined />} />
+        </Dropdown>
+      </div>
     </Header>
 
   )
