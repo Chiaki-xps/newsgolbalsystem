4555a512c0bfc128b74788df96e0f0fffd8da9fb
chiaki-xps <1490295446@qq.com>
2022-02-27 13:35:59 +0800

    58-状态管理-loading加载

--- src/components/sandbox/NewsRouter.js
+++ src/components/sandbox/NewsRouter.js
@@ -16,68 +16,77 @@ import Sunset from '../../views/sandbox/publish-manage/Sunset'
 import axios from 'axios'
 import NewsPreview from '../../views/sandbox/news-manage/NewsPreview'
 import NewsUpdate from '../../views/sandbox/news-manage/NewsUpdate'
+import {Spin } from 'antd'
 
+import {connect} from 'react-redux'
 const LocalRouterMap = {
-    "/home":Home,
-    "/user-manage/list":UserList,
-    "/right-manage/role/list":RoleList,
-    "/right-manage/right/list":RightList,
-    "/news-manage/add":NewsAdd,
-    "/news-manage/draft":NewsDraft,
-    "/news-manage/category":NewsCategory,
-    "/news-manage/preview/:id":NewsPreview,
-    "/news-manage/update/:id":NewsUpdate,
-    "/audit-manage/audit":Audit,
-    "/audit-manage/list":AuditList,
-    "/publish-manage/unpublished":Unpublished,
-    "/publish-manage/published":Published,
-    "/publish-manage/sunset":Sunset
+    "/home": Home,
+    "/user-manage/list": UserList,
+    "/right-manage/role/list": RoleList,
+    "/right-manage/right/list": RightList,
+    "/news-manage/add": NewsAdd,
+    "/news-manage/draft": NewsDraft,
+    "/news-manage/category": NewsCategory,
+    "/news-manage/preview/:id": NewsPreview,
+    "/news-manage/update/:id": NewsUpdate,
+    "/audit-manage/audit": Audit,
+    "/audit-manage/list": AuditList,
+    "/publish-manage/unpublished": Unpublished,
+    "/publish-manage/published": Published,
+    "/publish-manage/sunset": Sunset
 }
 
-export default function NewsRouter() {
+function NewsRouter(props) {
 
     const [BackRouteList, setBackRouteList] = useState([])
-    useEffect(()=>{
+    useEffect(() => {
         Promise.all([
             axios.get("/rights"),
             axios.get("/children"),
-        ]).then(res=>{
+        ]).then(res => {
             // console.log(res)
-            setBackRouteList([...res[0].data,...res[1].data])
+            setBackRouteList([...res[0].data, ...res[1].data])
             // console.log(BackRouteList)
         })
-    },[])
+    }, [])
 
-    const {role:{rights}} = JSON.parse(localStorage.getItem("token"))
+    const { role: { rights } } = JSON.parse(localStorage.getItem("token"))
 
-    const checkRoute = (item)=>{
+    const checkRoute = (item) => {
         return LocalRouterMap[item.key] && (item.pagepermisson || item.routepermisson)
     }
 
-    const checkUserPermission = (item)=>{
+    const checkUserPermission = (item) => {
         return rights.includes(item.key)
     }
 
     return (
-        <Switch>
-            {
-                BackRouteList.map(item=>
-                    {
+        <Spin size="large" spinning={props.isLoading}>
+            <Switch>
+                {
+                    BackRouteList.map(item => {
                         // if(有权限){}
                         // cheackRoute负责检查这个路由权限是否全部关掉了
                         // checkUserPermission检查资格
-                        if(checkRoute(item) && checkUserPermission(item)){
-                            return <Route path={item.key} key={item.key} component={LocalRouterMap[item.key]} exact/> 
+                        if (checkRoute(item) && checkUserPermission(item)) {
+                            return <Route path={item.key} key={item.key} component={LocalRouterMap[item.key]} exact />
                         }
                         return null
-                    }   
-                )
-            }
+                    }
+                    )
+                }
 
-            <Redirect from="/" to="/home" exact />
-            {
-                BackRouteList.length>0 && <Route path="*" component={Nopermission} />
-            }
-        </Switch>
+                <Redirect from="/" to="/home" exact />
+                {
+                    BackRouteList.length > 0 && <Route path="*" component={Nopermission} />
+                }
+            </Switch>
+        </Spin>
     )
 }
+
+const mapStateToProps = ({LoadingReducer:{isLoading}})=>({
+    isLoading
+  })
+
+export default connect(mapStateToProps)(NewsRouter)
