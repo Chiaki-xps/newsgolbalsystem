68f59d665ed8a3cc1d0789a7597373571ec46e34
chiaki-xps <1490295446@qq.com>
2022-02-27 20:45:48 +0800

    60-首页数据展示-排版

--- src/views/sandbox/home/Home.jsx
+++ src/views/sandbox/home/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-import { Button } from 'antd';
+import { Card, Col, Row, List,Avatar } from 'antd';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+const { Meta } = Card;
+
 // import axios from 'axios'
 export default function Home() {
 
+    // eslint-disable-next-line no-unused-vars
     const ajax = () => {
 
         // id获取到对应id数据
@@ -42,9 +46,52 @@ export default function Home() {
         //     console.log(res.data)
         // })
     }
+    
     return (
-        <div>
-            <Button type="primary" onClick={ajax}>Button</Button>
+        <div className="site-card-wrapper">
+            <Row gutter={16}>
+                <Col span={8}>
+                    <Card title="用户最常浏览" bordered={true}>
+                        <List
+                            size="small"
+                            // bordered
+                            dataSource={["1111", "222", "33333"]}
+                            renderItem={item => <List.Item>{item}</List.Item>}
+                        />
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card title="用户点赞最多" bordered={true}>
+                        <List
+                            size="small"
+                            // bordered
+                            dataSource={["1111", "222", "33333"]}
+                            renderItem={item => <List.Item>{item}</List.Item>}
+                        />
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card
+                        cover={
+                            <img
+                                alt="example"
+                                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                            />
+                        }
+                        actions={[
+                            <SettingOutlined key="setting" />,
+                            <EditOutlined key="edit" />,
+                            <EllipsisOutlined key="ellipsis" />,
+                        ]}
+                    >
+                        <Meta
+                            avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                            title="Card title"
+                            description="This is the description"
+                        />
+                    </Card>
+                </Col>
+            </Row>
         </div>
     )
 }
